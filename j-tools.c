5b770b85bf9c29a5df9f97bba008e2aef4ee8b2a
Dantae-Jekins <dantae.sona.jekins@gmail.com>
2022-04-14 00:58:40 -0300

    reorganized the linked list, it's actually suited for working now

--- j-tools.c
+++ j-tools.c
@@ -1,12 +1,26 @@
 // The sole purpose of this file is to mix all headers in j-tools files
 // and to test them myself.
 
-#include "./j-tools/j_strings.h"
+#include "./j-tools/j-structures/j_linkedList.h"
 #include <stdio.h>
 
 int main() {
-  char **ret = str_split("something", ',');
-  for(int i = 0; ret[i] != NULL; i++)
-    printf("%s\n", ret[i]);
-  return 0;
+
+  
+  list teste = list_new();
+  
+  struct Item item1 = item_new( 15 );
+  struct Item item2 = item_new( 25 );
+  
+  list_addFirst( &teste, item1);
+  list_addFirst( &teste, item2);
+  list_addLast( &teste, item1);
+  list_addLast( &teste, item2);
+
+  char *ret = list_toString(teste);
+  printf("%s", ret);
+  free(ret);
+
+  list_Free(&teste);
+
 }
--- j-tools/j-structures/j_linkedList.h
+++ j-tools/j-structures/j_linkedList.h
@@ -1,7 +1,7 @@
 #ifndef J_LINKEDLIST_H
 #define J_LINKEDLIST_H
 
-// V 1.1
+// V 1.2
 
 #include<stdlib.h>
 #include<stdbool.h>
@@ -9,173 +9,103 @@
 #include "../j_transform.h"
 
 
-// ESTRUTURAS DE MEMÓRIA PARA INTEIROS
-// Node de memória (int)
-struct int_Node
+// Estrutura do conteúdo de um node
+struct Item
 {
-    struct int_Node *next;
-    struct int_Node *prev;
-    int item;
-} typedef * int_n; 
+  int number;
+};
 
 
-// Lista ligada (int | HEAD)
+// Estrutura node
+struct Node
+{
+    struct Node *next;
+    struct Node *prev;
+    struct Item item;
+} typedef * snode; 
+
+
+// Estrura da lista ligada
 typedef struct 
 {
-    int_n ini;
-    int_n end;
+    snode ini;
+    snode end;
     int len;
-} int_ll;
+} list;
 
 
-// FUNÇÕES DE LISTAS PARA INTEIROS
+// -------------------------------------------------------------- //
+// FUNÇÕES SUJEITAS A MUDANÇA, NECESSÁRIO ADAPTAR DE PROJETO PARA PROJETO
 
-//Cria uma nova lista e retorna
-int_ll intll_new()
+// Cria um novo item
+// mudando os conteúdos de struct Item, se torna necessário
+// também mudar esta função
+struct Item item_new( int number )
 {
-  int_ll ret;
-  ret.len = 0;
-  ret.ini = NULL;
-  ret.end = NULL;
+  struct Item ret;
+  ret.number = number;
   return ret;
 }
 
 
-// Adiciona um elemento ao inicio da lista
-// é necessário liberar a memória
-void intll_add_First(int_ll * target, int value)
+// Função para comparar itens, 
+// mudando os conteúdos de struct Item, se torna necessário
+// também mudar esta função.
+bool __item_Compare(struct Item i1, struct Item i2)
 {
-  int_n new_n = malloc(sizeof( struct int_Node ));
-  new_n->item = value;
-  new_n->prev = NULL;
-  new_n->next = NULL;
+  if (i1.number == i2.number)
+    return true;
 
-  if ( target->len == 0 )
-  {
-    target->ini = new_n;
-    target->end = new_n;
-    target->len = 1;
-    return;
-  }
-
-  target->len += 1;
-  
-  int_n aux = target->ini;
-  target->ini = new_n;
-
-  new_n->next = aux;
-  aux->prev = new_n;
-}
-
-
-// Adiciona um elemento ao final da lista
-// é necessário liberar a memória
-void intll_add_Last(int_ll * target, int value)
-{
-  int_n new_n = malloc(sizeof( struct int_Node ));
-  new_n->item = value;
-  new_n->prev = NULL;
-  new_n->next = NULL;
-
-  if ( target->len == 0 )
-  {
-    target->ini = new_n;
-    target->end = new_n;
-    target->len = 1;
-    return;
-  }
-
-  target->len += 1;
-  
-  int_n aux = target->end;
-  target->end = new_n;
-  
-  aux->next = new_n;
-  new_n->prev = aux;
-}
-
-
-// Função apenas para ser utilizada pela biblioteca no quesito remoção
-void __intll_remove_node(int_ll *target, int_n node)
-{
-  int_n parent = node->prev;
-  int_n child = node->next;
-  
-  // faz o antecessor se conectar ao sucessor
-  if (parent != NULL)
-    parent->next = child;
-
-  else
-    target->ini = child;
-
-  
-  // faz o sucessor se conectar ao antecessor
-  if (child != NULL)
-    child->prev = parent;
-
-  else
-    target->end = parent;
-
-
-  free(node);
-}
-
-
-// Remove um elemento da lista, retorna true com sucesso
-bool intll_Remove( int_ll * target, int value) 
-{
-  // checa o tamanho da lista
-  if (target->len == 0) return false; 
-  
-  if (target->len == 1 ) 
-  {
-    if (target->ini->item == value)
-    {
-      __intll_remove_node( target, target->ini );
-      return true;
-    }
-    else return false;
-  }
-
-  // exitem mais de dois elementos:
-  int_n parent = (int_n) target;
-  while(parent->next != NULL)
-  {
-    if ( parent->next->item == value )
-    {
-      __intll_remove_node( target, parent->next);
-      return true;
-    }
-
-    // movimenta
-    parent = parent->next;
-  } 
-  
   return false;
 }
 
 
-// retorna a quantidade de elementos da lista
-int intll_Count( int_ll target )
+// Função para transformar itens em string,
+// mudando os conteúdos de struct Item, se torna necessário 
+// também mudar esta função
+char * __item_toString(struct Item i1)
 {
-  if (target.ini == NULL)
-    return 0;
-  
-  int_n current = target.ini;
-  int count = 1;
-  while ( current->next != NULL )
-  {
-    current = current->next;
-    count += 1;
-  }
+  char *ret;
+  ret = ret_str(i1.number);
 
-  return count;
+  return ret;
 }
 
 
-// transforma a lista em uma string, útil para printagem
-// é necessário liberar a memória
-char *intll_to_String( int_ll target)
+// Função que retorna uma cópia de item,
+// necessário saber um pouco mais sobre C para usar isto corretamente
+struct Item __item_Copy(struct Item value)
+{
+  // criação
+  struct Item ret;
+
+  // ret = value; só após C90 
+  ret.number = value.number;
+
+  return ret;
+  
+  // EM CASO DE PONTEIRO!
+  /*
+
+  Item { 
+    char * pointer;
+  }
+
+  Item __item_Copy( char * pointer ) {
+    // criação
+    Item ret;
+
+    ret.pointer = pointer !!!!!!!! ERRADO !!!!!!!!!
+    // você está criando uma cópia do ponteiro, não do conteúdo,
+    // faça uma função para copiar os conteúdos do ponteiro você mesmo
+  }
+  */
+}
+
+
+// Transforma a lista em uma string, útil para printagem
+// É necessário liberar a memória
+char *list_toString( list target)
 {
 
   // Checa se está vazia
@@ -195,14 +125,14 @@ char *intll_to_String( int_ll target)
     char *str2 = NULL;
 
     // percorre
-    int_n This = target.ini;
+    snode This = target.ini;
     while (1)
     {
       // libera value
       if ( value != NULL )
         free(value);
 
-      value = ret_str(This->item);
+      value = __item_toString(This->item);
 
       // concatena e libera a string anterior
       str2 = str_concac(str1, value);
@@ -229,15 +159,167 @@ char *intll_to_String( int_ll target)
 }
 
 
-// limpa a lista liberando toda sua memória
-void intll_Free( int_ll *target)
+
+
+
+
+// ------------------------------------------------------------- //
+/// FUNÇÕES NÃO SUJEITAS A MUDANÇA, NÃO TOQUE, SÓ EM CASO DE BUG
+
+//Cria uma nova lista e retorna
+list list_new()
 {
-  int_n node = target->ini;
-  int_n aux;
+  list ret;
+  ret.len = 0;
+  ret.ini = NULL;
+  ret.end = NULL;
+  return ret;
+}
+
+
+// Adiciona um elemento ao inicio da lista
+// é necessário liberar a memória
+void list_addFirst(list * target, struct Item value)
+{
+  snode new_n = (snode) malloc(sizeof( struct Node ));
+  new_n->item = __item_Copy(value);
+  new_n->prev = NULL;
+  new_n->next = NULL;
+
+  if ( target->len == 0 )
+  {
+    target->ini = new_n;
+    target->end = new_n;
+    target->len = 1;
+    return;
+  }
+
+  target->len += 1;
+  
+  snode aux = target->ini;
+  target->ini = new_n;
+
+  new_n->next = aux;
+  aux->prev = new_n;
+}
+
+
+// Adiciona um elemento ao final da lista
+// é necessário liberar a memória
+void list_addLast(list * target, struct Item value)
+{
+  snode new_n = (snode) malloc(sizeof( struct Node ));
+  new_n->item = __item_Copy(value);
+  new_n->prev = NULL;
+  new_n->next = NULL;
+
+  if ( target->len == 0 )
+  {
+    target->ini = new_n;
+    target->end = new_n;
+    target->len = 1;
+    return;
+  }
+
+  target->len += 1;
+  
+  snode aux = target->end;
+  target->end = new_n;
+  
+  aux->next = new_n;
+  new_n->prev = aux;
+}
+
+
+// Função para remover um nó com um nó como parâmetro
+// O nó DEVE existir na lista, não é uma função de busca
+void list_nodeRemove(list *target, snode node)
+{
+  snode parent = node->prev;
+  snode child = node->next;
+  
+  // faz o antecessor se conectar ao sucessor
+  if (parent != NULL)
+    parent->next = child;
+
+  else
+    target->ini = child;
+
+  
+  // faz o sucessor se conectar ao antecessor
+  if (child != NULL)
+    child->prev = parent;
+
+  else
+    target->end = parent;
+
+  target->len -= 1;
+  free(node);
+}
+
+
+// Função para remover um nó com uma estrutura como parâmetro
+// Retorna true se foi removido com sucesso
+bool list_itemRemove( list * target, struct Item value) 
+{
+  // checa o tamanho da lista
+  if (target->len == 0) return false; 
+  
+  if (target->len == 1 ) 
+  {
+    if (__item_Compare( target->ini->item, value))
+    {
+      list_nodeRemove( target, target->ini );
+      return true;
+    }
+    else return false;
+  }
+
+  // exitem mais de dois elementos:
+  snode parent = (snode) target;
+  while(parent->next != NULL)
+  {
+    if (__item_Compare( target->ini->item, value))
+    {
+      list_nodeRemove( target, parent->next);
+      return true;
+    }
+
+    // movimenta
+    parent = parent->next;
+  } 
+  
+  return false;
+}
+
+
+// Retorna a quantidade de elementos da lista
+int list_Count( list target )
+{
+  if (target.ini == NULL)
+    return 0;
+  
+  snode current = target.ini;
+  int count = 1;
+  while ( current->next != NULL )
+  {
+    current = current->next;
+    count += 1;
+  }
+
+  return count;
+}
+
+
+// Limpa a lista liberando toda sua memória
+void list_Free( list *target)
+{
+  snode node = target->ini;
+  snode aux;
   while ( node != NULL )
   {
     aux = node->next;
-    __intll_remove_node(target, node);
+    list_nodeRemove(target, node);
     node = aux;
   }
 }
